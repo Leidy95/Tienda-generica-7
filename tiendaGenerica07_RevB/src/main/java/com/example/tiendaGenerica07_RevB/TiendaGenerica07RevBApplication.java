151abc4c4801986193cd01ffad525e0258b119b2
edfes <edfes@LaptopEFS>
2021-09-19 22:15:08 -0500

    Esta el la primera labor para la creacion del proyecto TiendaGenerica

---
+++ tiendaGenerica07_RevB/src/main/java/com/example/tiendaGenerica07_RevB/TiendaGenerica07RevBApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiendaGenerica07_RevB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaGenerica07RevBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaGenerica07RevBApplication.class, args);
+	}
+
+}
---
+++ tiendaGenerica07_RevB/src/test/java/com/example/tiendaGenerica07_RevB/TiendaGenerica07RevBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiendaGenerica07_RevB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaGenerica07RevBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
